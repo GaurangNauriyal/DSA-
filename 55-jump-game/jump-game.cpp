ce9902c9abbfb671407b2b9023c26cd5e1eea5b6
Gaurang Nauriyal <gaurangnauriyal45@gmail.com>
2026-09-16 19:43:56 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (25.94%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int canReach = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > canReach) 
+                return false;
+            canReach = max(canReach, i + nums[i]);
+        }
+        return true;
+    }
+};
