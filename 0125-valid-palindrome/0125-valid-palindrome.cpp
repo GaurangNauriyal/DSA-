683ab6ace8fd5e3801ef1d977fdd812de269e279
Gaurang Nauriyal <gaurangnauriyal45@gmail.com>
2026-09-21 21:24:44 +0530

    Time: 3 ms (27.63%), Space: 9.7 MB (93.77%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int a=0;
+        int b=s.size()-1;
+        while(a<=b){
+           if(!isalnum(s[a])){a++; continue;}
+           if(!isalnum(s[b])){b--;continue;}
+           if(tolower(s[a])!=tolower(s[b]))return false;
+           else{
+               a++;
+               b--;
+           }
+       }
+       return true;
+    }
+};
